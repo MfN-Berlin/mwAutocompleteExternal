0949ba6e379642a7a5967ab6978cb9076e17d8ca
Alvaro Ortiz-Troncoso <webmaster@kultur-und-stress.de>
2015-10-08 15:28:08 +0200

    Added autocompleter for German university names

--- src/connectors/Wikispecies.php
+++ src/connectors/Wikispecies.php
@@ -1,56 +1,6 @@
 <?php
 namespace mwAutocompleteExternal\connectors;
-
-interface Autocompleter {
-	/**
-	 * Submit a query to the web service 
-	 * and parse the results into a string
-	 * suitable for autocompletion.
-	 * 
-	 * @param unknown $query
-	 */
-	function search( $query );
-}
-
-abstract class AbstractAutocompleter {
-	/** Instance of the Snoopy HTTP client */
-	protected $snoopy;
-	
-	/**
-	 * Initialize the class. Call from child class
-	 * # Inject the Snoopy (HTML client) instance
-	 * # Set the base URL
-	 */
-	protected function init( $snoopy ) {
-		// The Snoopy instance
-		$this->snoopy = $snoopy;
-	}
-
-	/**
-	 * Submit them to the web service and return the raw (unparsed) result 
-	 * 
-	 * @param String $searchUrl
-	 */
-	protected function submit( $searchUrl ) {
-		$this->snoopy->submit( $searchUrl );
-		return $this->snoopy->results;
-	}
-	
-	/**
-	 * Format an array of results into an autocompletion string
-	 * 
-	 * @param array $parts
-	 */
-	protected function format( array $parts ) {
-		$resp = '{"sfautocomplete": [';
-		for( $i = 0; $i < count( $parts ); $i++ ) {
-			$resp .= '{ "title" : ' . $parts[ $i ] . '},';
-		}
-		$resp = substr( $resp, 0, -1 );
-		$resp .= ']}';
-		return $resp;
-	}
-} 
+include __DIR__ . "/Autocompleter.php";
 
 /**
  * Queries Wikispecies for taxon names.
